47954b3ae39217f96440e95eb554cca1c64c5c50
Vishesh Vadhera <vishesh.2710@gmail.com>
2017-10-05 00:59:12 +0530

    Add workservice as superclass of LangaugeServer

--- src/java_tools/codecompletion/java/com/google/codecompletion/SkylarkLanguageServer.java
+++ src/java_tools/codecompletion/java/com/google/codecompletion/SkylarkLanguageServer.java
@@ -1,17 +1,27 @@
 package src.java_tools.codecompletion.java.com.google.codecompletion;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.eclipse.lsp4j.CompletionOptions;
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
 import org.eclipse.lsp4j.InitializeParams;
 import org.eclipse.lsp4j.InitializeResult;
 import org.eclipse.lsp4j.InitializedParams;
 import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.SymbolInformation;
+import org.eclipse.lsp4j.WorkspaceSymbolParams;
 import org.eclipse.lsp4j.services.LanguageServer;
 import org.eclipse.lsp4j.services.TextDocumentService;
 import org.eclipse.lsp4j.services.WorkspaceService;
 
-public class SkylarkLanguageServer implements LanguageServer {
+public class SkylarkLanguageServer
+    implements LanguageServer, WorkspaceService {
+
+  private static final int FORCED_EXIT_CODE = 1;
 
   @Override
   public CompletableFuture<InitializeResult> initialize(InitializeParams initializeParams) {
@@ -36,7 +46,9 @@ public class SkylarkLanguageServer implements LanguageServer {
 
   @Override
   public void exit() {
-
+    Executors.newSingleThreadScheduledExecutor().schedule(() -> {
+      System.exit(FORCED_EXIT_CODE);
+    }, 1, TimeUnit.MINUTES);
   }
 
   @Override
@@ -46,6 +58,22 @@ public class SkylarkLanguageServer implements LanguageServer {
 
   @Override
   public WorkspaceService getWorkspaceService() {
+    return this;
+  }
+
+  @Override
+  public CompletableFuture<List<? extends SymbolInformation>> symbol(
+      WorkspaceSymbolParams workspaceSymbolParams) {
     return null;
+  }
+
+  @Override
+  public void didChangeConfiguration(DidChangeConfigurationParams didChangeConfigurationParams) {
+
+  }
+
+  @Override
+  public void didChangeWatchedFiles(DidChangeWatchedFilesParams didChangeWatchedFilesParams) {
+
   }
 }
